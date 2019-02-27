c1e8448b0046caeca28b881486d0fb03e547f3a2
mayank-26 <bardwajmayank@gmail.com>
2019-02-28 03:21:34 +0530

    max of 2 numbers

---
+++ 2max.java
@@ -0,0 +1,49 @@
+class Smax
+{
+	public static void main(String args[])
+	{
+	
+	 int a=1,b=5,c=3,smax,max;
+	 if(a>b)
+	 {max=a;
+	 smax=b;
+	 
+	 }
+	 else{
+	 	max=b;
+	 	smax=a;}
+	 	
+	 	
+	 	
+	 	
+	 	
+	if(c>max)
+	{
+		smax=max;
+		max=c;}
+    else{
+    	if(c>smax)
+    	smax=c;
+    	else {
+    	}
+    	
+    	
+    	
+    }
+	
+		
+		System.out.println(smax);
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
